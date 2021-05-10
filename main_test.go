33fc3aef1e856bdae5aec231b4db9fec433ccf04
noppakao <noppakao_9@windowslive.com>
2021-05-10 17:22:17 +0700

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
-
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
 	"testing"
+
+	_ "github.com/lib/pq"
 )
 
 func TestThis(t *testing.T) {
@@ -11,4 +15,27 @@ func TestThis(t *testing.T) {
 
 func TestThis2(t *testing.T) {
 
+}
+
+// a Test that tries to connect to a PostgreSQL database
+func TestConnect(t *testing.T) {
+	// First get some ENV variabeles
+	host := os.Getenv("POSTGRES_HOST")
+	user := os.Getenv("POSTGRES_USER")
+	port := os.Getenv("POSTGRES_PORT")
+	password := os.Getenv("POSTGRES_PASS")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	psqlConnection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open the DB connection
+	db, err := sql.Open("postgres", psqlConnection)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Make sure connection really works
+	err = db.Ping()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
